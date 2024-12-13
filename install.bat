03108d9bde572fb3539ece40d2d1f1024cc7602c
Violet-R4iN <frostscarlet123@gmail.com>
2024-12-13 13:23:17 +0700

    y

--- install.bat
+++ install.bat
@@ -15,12 +15,3 @@ IF %ERRORLEVEL% NEQ 0 (
     echo Failed to install dependencies. Exiting...
     exit /b 1
 )
-
-REM Create a shortcut to the Python script on the Desktop
-echo Creating shortcut for %PYTHON_SCRIPT% on the desktop...
-powershell -NoProfile -Command ^
-  "$ws = New-Object -ComObject WScript.Shell; $s = $ws.CreateShortcut('%DESKTOP%\%SHORTCUT_NAME%.lnk'); $s.TargetPath = '%~dp0%PYTHON_SCRIPT%'; $s.Save();"
-
-REM Completion message
-echo Shortcut created successfully. Script completed!
-pause
